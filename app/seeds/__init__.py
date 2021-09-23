035422ca8873099ca619168e355ae47c58a42523
Cam Chandler <camchandler15@gmail.com>
2021-09-23 08:55:26 -0400

    made more robust user seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,5 @@
 from flask.cli import AppGroup
-from .users import seed_users, undo_users
+from .users import seed_users, seed_base_users,undo_users
 from .hearts import seed_hearts, undo_hearts
 
 # Creates a seed group to hold our commands
@@ -10,6 +10,7 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    seed_base_users()
     seed_users()
     seed_hearts()
     # Add other seed functions here
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,8 +1,13 @@
 from app.models import db, User
-
+from faker import Faker
+Faker.seed(0)
+fake = Faker()
+fake_latin = Faker('la')
+fake_english = Faker('en-US')
+fake_japanese = Faker('ja-JP')
 
 # Adds a demo user, you can add other users here if you want
-def seed_users():
+def seed_base_users():
     Romeo = User(
         username='Romeow',
         email= 'juliet@romeo.com',
@@ -22,6 +27,30 @@ def seed_users():
 
     db.session.commit()
 
+def seed_users(seeds=30):
+
+    for i in range(seeds):
+        isJapanese =  fake.boolean(chance_of_getting_true=20)
+
+        username = fake_japanese.user_name() if isJapanese else fake_english.user_name()
+
+        display_name = fake_japanese.romanized_name() if isJapanese else fake_english.name_nonbinary()
+
+        email = fake_japanese.email() if isJapanese else fake_english.user_name()
+
+        password = 'password'
+
+        profile_picture_url = fake.image_url()
+
+        user = User(
+            username= username,
+            display_name=display_name,
+            email=email,
+            profile_picture_url = profile_picture_url,
+            password=password
+        )
+
+        db.session.add(user)
 
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
