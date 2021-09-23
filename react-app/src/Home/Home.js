273533787d8a5a7818ca24c8570b1227b0f7dfa6
Cam Chandler <camchandler15@gmail.com>
2021-09-23 14:48:29 -0400

    fixed style conflict

--- react-app/src/Home/Home.js
+++ react-app/src/Home/Home.js
@@ -1,6 +1,7 @@
 import CreateHeartForm from "../components/Hearts/CreateHeartForm"
 import './Home.css'
-import './HomeCreateHeart.css'
+import HomeCreateHeartFormStyle from "./HomeCreateHeartStyle"
+// import './HomeCreateHeart.css'
 const Home = () => {
 
     return (
@@ -8,9 +9,11 @@ const Home = () => {
         <div className ='home-header' >
             <span>Home</span>
         </div>
+        <HomeCreateHeartFormStyle>
         <div className = 'create-heart-form-container'>
         <CreateHeartForm />
         </div>
+        </HomeCreateHeartFormStyle>
         <div className = 'hearts-page'></div>
         </div>
     )
