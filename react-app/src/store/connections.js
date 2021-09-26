bad6e4fd915f051a310b95be9af73d078a00fff2
Cam Chandler <camchandler15@gmail.com>
2021-09-26 19:37:48 -0400

    thunk to delete connection

--- react-app/src/store/connections.js
+++ react-app/src/store/connections.js
@@ -1,5 +1,8 @@
-const GET = 'conneections/GET'
+const GET = 'connections/GET'
 const ADD = 'connections/ADD'
+const DELETE = 'connections/DELTE'
+
+
 const get = (connections) => ({
     type: GET,
     connections
@@ -9,6 +12,10 @@ const add = (conenction) => ({
     conenction
 })
 
+const deleteCon = (connectionId) => ({
+    type:DELETE,
+    connectionId
+})
 export const getConnections = (userId) => async (dispatch) => {
     let res = await fetch(`/api/connections/user/${userId}`)
 
@@ -36,6 +43,25 @@ export const addConnection = (user_one, user_two) => async (dispatch) => {
         dispatch(add(data))
     }
 }
+
+
+export const deleteConnection = (user_one, user_two) => async (dispatch) => {
+    let res = await fetch('/api/connections/',{
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({
+            user_one,
+            user_two
+        })
+    })
+
+    if(res.ok){
+        let data = await res.json()
+        dispatch(deleteCon(data))
+    }
+}
 const initialState = {}
 
 const connectionReducer = (state = initialState , action) => {
@@ -44,6 +70,14 @@ const connectionReducer = (state = initialState , action) => {
             return {...state, ...action.connections}
         case ADD:
             return {...state, ...action.conenction}
+        case DELETE:
+            let newState = {...state}
+
+            if(newState[action.connectionId]){
+                delete newState[action.connectionId]
+            }
+            return{...newState}
+
         default:
             return {...state}
     }
