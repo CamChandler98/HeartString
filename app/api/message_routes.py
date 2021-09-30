1f058cad3e0e592f65e98eea4a3cd3b0cfbeae47
Cam Chandler <camchandler15@gmail.com>
2021-09-30 09:44:48 -0400

    create notif when message is created

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -1,6 +1,7 @@
 from app.forms.message_form import MessageForm
 from flask import Blueprint, request
 from app.models import Message, User, db
+from app.models.message_notification import MessageNotification
 from app.models.user import user_connections
 from sqlalchemy import or_
 from app.api.auth_routes import validation_errors_to_error_messages
@@ -19,7 +20,7 @@ def get_conversation():
 
     user_one_id = data['user_one_id']
     user_two_id = data['user_two_id']
-    
+
 
 
     messages = Message.query.filter(or_(Message.user_id == user_one_id, Message.user_id == user_two_id)).filter(or_(Message.receiver_id == user_one_id, Message.receiver_id == user_two_id))
@@ -38,6 +39,13 @@ def create_message():
             user_id = form.data['user_id'],
             receiver_id = form.data['receiver_id'],
         )
+
+        message_notification = MessageNotification(
+            user_id = form.data['receiver_id'],
+            sender_id = form.data['user_id'],
+        )
+
+        db.session.add(message_notification)
 
         db.session.add(message)
         db.session.commit()
