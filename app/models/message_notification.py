860db3ff79abc1d5a70f86ee6dd106f4e921f49b
Cam Chandler <camchandler15@gmail.com>
2021-09-30 09:21:36 -0400

    made model for notification

---
+++ app/models/message_notification.py
@@ -0,0 +1,27 @@
+from .db import db
+from .user import User
+import datetime
+now = datetime.datetime.now()
+class MessageNotification ():
+
+    __tablename__ = 'messsage_notifications'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    sender_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    seen = db.Column(db.Boolean(), default= False)
+    created_at = db.Column(db.DateTime(), default= now)
+
+
+    user = db.relationship("User", back_populates = 'received_notifications', foreign_keys='Message.user_id')
+
+    sender = db.relationship("User", back_populates = 'sent_notifications', foreign_keys='Message.receiver_id')
+
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "user_id": self.user_id,
+            "sender_id": self.sender_id,
+            "seen": self.seen
+        }
