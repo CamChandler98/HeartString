5381bb375904d8ab49bc4b6e9faa88234a4cb30b
Cam Chandler <camchandler15@gmail.com>
2021-11-04 14:52:12 -0400

    refactor reply route

--- app/api/notification_routes.py
+++ app/api/notification_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request
 from app.models import db
 from app.models.message_notification import MessageNotification
+from app.models.reply_notification import ReplyNotification
+
 
 notification_routes = Blueprint('notifications', __name__)
 
@@ -16,6 +18,24 @@ def user_message_notifications(id):
 @notification_routes.route('/message/<int:id>', methods = ['DELETE'])
 def see_notification(id):
     notification = MessageNotification.query.get(id)
+
+    db.session.delete(notification)
+
+    db.session.commit()
+
+    return {'deleted': notification.id}
+
+@notification_routes.route('/reply/user/<int:id>')
+def user_reply_notifications(id):
+
+    notifications = ReplyNotification.query.filter(ReplyNotification.user_id == id).filter(ReplyNotification.seen == False)
+
+    return {notification.id:notification.to_dict() for notification in notifications}
+
+
+@notification_routes.route('/reply/<int:id>', methods = ['DELETE'])
+def see_notification(id):
+    notification = ReplyNotification.query.get(id)
 
     db.session.delete(notification)
 
