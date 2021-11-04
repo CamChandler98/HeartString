cca46b8db22469631fc3962effb4b786c7e173c5
Cam Chandler <camchandler15@gmail.com>
2021-11-04 14:50:34 -0400

    add reply notification routes

---
+++ app/api/reply_notification_routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request
+from app.models import db
+from app.models.reply_notification import ReplyNotification
+
+notification_routes = Blueprint('reply_notifications', __name__)
+
+
+@notification_routes.route('/replies/user/<int:id>')
+def user_reply_notifications(id):
+
+    notifications = ReplyNotification.query.filter(ReplyNotification.user_id == id).filter(ReplyNotification.seen == False)
+
+    return {notification.id:notification.to_dict() for notification in notifications}
+
+
+@notification_routes.route('/message/<int:id>', methods = ['DELETE'])
+def see_notification(id):
+    notification = ReplyNotification.query.get(id)
+
+    db.session.delete(notification)
+
+    db.session.commit()
+
+    return {'deleted': notification.id}
